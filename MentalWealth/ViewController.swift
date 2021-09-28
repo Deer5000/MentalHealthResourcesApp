fa79d7522fee74073197d94c95f2997b4943509d
deer5000 <khidrbrinkley32@gmail.com>
2021-09-28 17:37:07 -0400

    Initial Commit

---
+++ MentalWealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentalWealth
+//
+//  Created by Khidr Brinkley on 9/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
